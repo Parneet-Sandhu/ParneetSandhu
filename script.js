84e396335c430c3b50f9040f231bdce4e5dcbe28
Parneet Kaur Sandhu <121374723+Parneet-Sandhu@users.noreply.github.com>
2024-11-16 15:18:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I')) {
+        e.preventDefault();
+    }
+});
